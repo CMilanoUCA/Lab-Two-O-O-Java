ea39bfe068c8701b61c219ca2caf7f23d59e75e3
CMilanoUCA <cmilano@cub.uca.edu>
2025-02-17 16:47:38 -0600

    Fix up ListPanel and EventModel for questioning

--- src/AddEventModel.java
+++ src/AddEventModel.java
@@ -1,6 +1,48 @@
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class AddEventModel extends JDialog {
     // The purpose of this class is to add an Event—either a Deadline or a Meeting to the planner.
+
+    private EventListPanel eventListPanel;
+    private JButton addEventButton;
+    AddEventModel myself;
+
+    /*
+    AddEventModel modal;
+    record Attribute(String name, JComponent value) {}
+    ArrayList<Attribute> attributes;
+    JPanel infoCollectorPanel;
+    JComboBox<String> eventTypeComboBox;
+    */
+
+    public AddEventModel(EventListPanel eventListPanel) {
+        myself = this;
+        this.eventListPanel = eventListPanel;
+        this.setTitle("Add Event");
+        this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        this.add(new addEventPanel());
+        this.pack();
+        this.setVisible(true);
+    }
+
+    private class addEventPanel extends JPanel {
+        public addEventPanel() {
+            addEventButton = new JButton("Add Event");
+            addEventButton.addActionListener(e -> {
+                Meeting meeting = new Meeting(meeting.getName(),
+                        meeting.getDateTime(), meeting.getEndDateTime(),
+                        meeting.getLocation());
+                eventListPanel.addEvent(meeting);
+                myself.dispose();
+            });
+
+            add(addEventButton);
+
+            setPreferredSize(new Dimension(500, 500));
+            setBackground(Color.CYAN);
+        }
+    }
 
 }
--- src/EventListPanel.java
+++ src/EventListPanel.java
@@ -1,16 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.function.Predicate;
 
 public class EventListPanel extends JPanel {
-    EventListPanel() {  // Constructor?
-        events = new ArrayList<>();
-        controlPanel = new JPanel();
-        displayPanel = new JPanel();
-        sortDropDown = new JComboBox();
-        filterDisplay = new JCheckBox("Filter Display");
-        addEventButton = new JButton("Add Event");
-    }
-
 
     ArrayList<Event> events; // a list of the events to be listed.
 
@@ -19,13 +13,75 @@ public class EventListPanel extends JPanel {
     JPanel displayPanel;     // a panel to hold the EventPanels corresponding to the events.
 
     JComboBox sortDropDown;  // A drop down that will sort the events in the list
-                             // by name, date, or by the same qualities but in reverse order.
-                             // Add the listener for this comboBox as using a lambda function.
+    // by name, date, or by the same qualities but in reverse order.
+    // Add the listener for this comboBox as using a lambda function.
 
     JCheckBox filterDisplay; // a set of checkboxes that allow the user to filter the
-                             // displayed events, removing complete tasks, deadlines, or meetings.
-                             // Add the listener for the filterDisplay as an anonymous class.
+    // displayed events, removing complete tasks, deadlines, or meetings.
+    // Add the listener for the filterDisplay as an anonymous class.
 
     JButton addEventButton;   // a button that opens an AddEventModal.
 
+    Map<String, Predicate<Event>> filters;
+    ArrayList<JCheckBox> filterBoxes;
+
+    /*
+    EventListPanel() {  // Constructor
+        events = new ArrayList<>();
+        controlPanel = new JPanel();
+        displayPanel = new JPanel();
+        sortDropDown = new JComboBox();
+        filterDisplay = new JCheckBox("Filter Display");
+        addEventButton = new JButton("Add Event");
+    }
+    */
+
+
+    public EventListPanel() {
+        setPreferredSize(new Dimension(1000, 750));
+        setBackground(Color.BLACK);
+
+        // Set Control Panel Size
+        controlPanel = new JPanel();
+        controlPanel.setPreferredSize(new Dimension(850, 500));
+        events = new ArrayList<>();
+
+        // Add Event Button
+        addEventButton = new JButton("Add Event");
+        addEventButton.setFont(new Font("Tahoma", Font.BOLD, 14));
+        addEventButton.addActionListener(e -> {
+            new AddEventModel(this);
+        });
+        controlPanel.add(addEventButton);
+        add(controlPanel);
+
+        // Display Panel to Display Events
+        displayPanel = new JPanel();
+        displayPanel.setPreferredSize(new Dimension(850, 650));
+        add(displayPanel);
+    }
+
+    public void addEvent(Event event) { // Adds an Event
+        events.add(event);
+        updateDisplay();
+    }
+
+    public boolean isComplete(Event event) {
+        boolean isComplete = true;
+        for (JCheckBox filter : filterBoxes) {
+            if (filter.isSelected()) {
+                Predicate<Event> pred = filters.get(filter.getText());
+            }
+        }
+        return isComplete;
+    }
+
+    public void updateDisplay() {
+        displayPanel.removeAll();
+        for (Event event : events) {
+            displayPanel.add(new EventPanel(event));
+        }
+        revalidate();
+        repaint();
+    }
 }
